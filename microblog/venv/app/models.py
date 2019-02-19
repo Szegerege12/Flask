0ba1ea23a9c72ea090a800917281648cfae9c664
Marek Sieńko <mrszegerege@gmail.com>
2019-02-19 16:24:31 +0100

    0,13 - blueprints...

--- microblog/venv/app/models.py
+++ microblog/venv/app/models.py
@@ -1,20 +1,20 @@
 from datetime import datetime
-from time import time
-import jwt
-from app import db, login, app
-from werkzeug.security import generate_password_hash, check_password_hash   # moduly do tworzenia hasel i sprawdzannia po hashu
-from flask_login import UserMixin
 from hashlib import md5
+from time import time
+from flask import current_app
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+from app import db, login
 
 
-# tabela pomocnicza przechowująca jedynie klucze obce, więc nie ma sensu robienia jej na modelu klasy
-followers = db.Table('followers',
+followers = db.Table(
+    'followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
 )
 
 
-# klasa użytkownika
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -29,70 +29,55 @@ class User(UserMixin, db.Model):
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
-# metody klasy User
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
     def set_password(self, password):
-        """Funkcja tworzy haslo hash"""
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-        """Funkcja sprawdzajaca zgodnosc hasla"""
         return check_password_hash(self.password_hash, password)
 
     def avatar(self, size):
-        """Funkcja generująca avatar na podstawie emaila za pomocą
-        strony gravatar."""
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
 
-    def follow(self,user):
-        """Metoda słuząca do followania usera wraz z metoda wspomagajaca"""
+    def follow(self, user):
         if not self.is_following(user):
             self.followed.append(user)
 
-    def unfollow(self,user):
-        """Metoda do przestanai sledzenia uzytkownika z metoda wspomagajaca"""
+    def unfollow(self, user):
         if self.is_following(user):
             self.followed.remove(user)
 
-    def is_following(self,user):
-        """Sprawdza czy istnieje relacja follow miedzy userami"""
+    def is_following(self, user):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
-        """Łaczy ze sobą sledzonych uzytkowników wraz z ich postami.
-        Baza danych ma tworzyć tymczasową tabele, która łączy dane
-        z tabel i postów obserwatorów. Za pomocą operatora "union'
-        łaczę dwa zapytania w jedno( posty followed osób i moje własne
-        """
         followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
-            followers.c.follower_id == self.id)
+                followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
     def get_reset_password_token(self, expires_in=600):
-        """Generuje token JWT jako ciąg znakow. Kodowanie utf-8 jest
-        konieczne ponieważ fukncja zwraca token jako ciąg bajtów
-        """
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+            current_app.config['SECRET_KEY'],
+            algorithm='HS256').decode('utf-8')
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY'],
-                            algorithm=['HS256'])['reset_password']
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
         except:
             return
         return User.query.get(id)
 
-#  loader uzyytkowniów
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
@@ -103,9 +88,7 @@ class Post(db.Model):
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    language = db.Column(db.String(5))
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
-
-
-
--- microblog/venv/config.py
+++ microblog/venv/config.py
@@ -1,10 +1,13 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI= os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
@@ -13,6 +16,6 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
-    POSTS_PER_PAGE = 3  # posty wyswietlane na stornie ich liczba
-    LANGUAGES = ['en', 'pl']
-
+    LANGUAGES = ['en', 'es']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    POSTS_PER_PAGE = 25
